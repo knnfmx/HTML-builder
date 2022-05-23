cae1f34c5bb92f3be988ed59adcbf1133630e374
kennyfmx <fmxhighlife@gmail.com>
2022-05-23 18:09:05 +0300

    feat: add function for write text in file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,6 +3,6 @@ const path = require('path');
 const string = path.dirname(__filename);
 console.log(string);
 
-const stream = fs.createReadStream(`${string}`+ '/text.txt', 'utf8');
+const stream = fs.createReadStream(`${string}` + '/text.txt', 'utf8');
 stream.on('data', (data) => console.log(data));
 stream.on('error', (err) => console.log(`Err: ${err}`));
---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout} = process;
+let dir = path.dirname(__filename);
+let file = 'text.txt';
+stdout.write('Give me a message:\n');
+stdin.on('data', (data) => {
+  if(process.on('SIGINT', () => {
+    process.exit();
+  }));
+  fs.createWriteStream(`${dir}/${file}`).write(data);
+});
+process.on('exit', (code) => {
+  if (code === 0) {
+    stdout.write('See ya!');
+  } else {
+    stdout.write(`error: ${code}`);
+  }
+});
