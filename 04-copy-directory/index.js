770a2684928b9dd9f101ecff83e3157f886b4549
kennyfmx <fmxhighlife@gmail.com>
2022-05-23 20:41:10 +0300

    feat: add function to copy files from a folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+let dir = path.join(__dirname, 'files');
+console.log(dir);
 
+let dirForCopy = path.join(__dirname, 'files-copy/');
+
+fs.mkdir(dirForCopy, {recursive: true}, error => error);
+async function makeCopy(dir) {
+  fs.readdir(dir, (error, files) => {
+    files.forEach(file => {
+      fs.copyFile(`${dir}/${file}`, `${dirForCopy}${file}`, error => error);
+    })
+  });
+}
+makeCopy(dir);
